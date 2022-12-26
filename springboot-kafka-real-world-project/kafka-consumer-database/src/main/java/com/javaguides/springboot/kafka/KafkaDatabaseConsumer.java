dfeb6a2382579f8e21f3cd78907aa45259579a18
harsha-commit <harshavardhan.bashavathini@gmail.com>
2022-12-26 17:55:03 +0530

    Completed the project, eveything is working as expected.

--- springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/com/javaguides/springboot/kafka/KafkaDatabaseConsumer.java
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/com/javaguides/springboot/kafka/KafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.javaguides.springboot.kafka;
 
+import com.javaguides.springboot.entity.WikimediaData;
+import com.javaguides.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,11 +12,11 @@ public class KafkaDatabaseConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
-    //private WikimediaDataRepository dataRepository;
+    private WikimediaDataRepository dataRepository;
 
-//    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
-//        this.dataRepository = dataRepository;
-//    }
+    public KafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
 
     @KafkaListener(
             topics = "${spring.kafka.topic.name}",
@@ -24,9 +26,9 @@ public class KafkaDatabaseConsumer {
 
         LOGGER.info(String.format("Event message received: %s", eventMessage));
 
-//        WikimediaData wikimediaData = new WikimediaData();
-//        wikimediaData.setWikiEventData(eventMessage);
-//
-//        dataRepository.save(wikimediaData);
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepository.save(wikimediaData);
     }
 }
---
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/com/javaguides/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.javaguides.springboot.repository;
+
+import com.javaguides.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
